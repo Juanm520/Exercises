98637a78895a279086ae7c0eea807f80a7a005ce
Juanm520 <juanma_520@hotmail.com>
2022-08-18 21:57:43 -0500

    Test Git on VSCode

--- ExcercisesPHP/Form.php
+++ ExcercisesPHP/Form.php
@@ -8,9 +8,16 @@
 </head>
 <body>
 
-    <form action="" method="get">
+    <form action="./server.php" method="get">
 
-        
+        <label for="name">Enter your fullname</label>
+        <input type="text" name="name" id="name">
+
+        <label for="age">Enter your age</label>
+        <input type="text" name="age" id="age">
+
+        <button type="submit">Submit Form</button>
+
 
     </form>
 
