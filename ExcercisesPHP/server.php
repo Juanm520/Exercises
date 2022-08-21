bd23aa035849265d52a3a032ea6f1b7c927fd49a
Juanm520 <juanma_520@hotmail.com>
2022-08-21 11:35:54 -0500

    Final file, example methof GET in Forms

--- ExcercisesPHP/server.php
+++ ExcercisesPHP/server.php
@@ -1,7 +1,11 @@
 <?php
 
-echo "<pre>";
-
-var_dump($_GET);
-
-echo "</pre>";
+$name = $_GET["name"];
+$age = $_GET["age"];
+ 
+if ($name = "NULL" || $age= "NULL") {
+    echo "No hay datos para la solicitud";
+}
+else {
+    echo "Al parecer eres $name y tienes $age años, puedes acceder a pagar impuestos."; 
+}
