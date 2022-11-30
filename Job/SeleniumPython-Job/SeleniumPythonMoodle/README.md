ac80dd775b3da443bb71d8b410afedbf7f931e4a
Juanm520 <juanma_520@hotmail.com>
2022-11-30 13:58:54 -0500

    Adde Job Selenium in Python code.

---
+++ Job/SeleniumPython-Job/SeleniumPythonMoodle/README.md
@@ -0,0 +1,15 @@
+# Selenium en Python 
+
+#Moodle
+
+Automatización de tareas para cumplir requisitos específicos en cursos de Moodle.
+
+El codigo en este repositorio esta escrito en Python usando la libreria Selenium para manipular mediante automatización
+los parametros dentro de Moodle LMS de la institución educativa en la que trabajo. En ese momento necesitaba hacer cambios
+masivos rapidamente y descargar calificaciones para generar los reportes de estudiantes en periodos bimestrales.
+
+Son funcionales, pero algunos deben ser actualizados a la versión mas reciente de Selenium, principalmente algunos metodos 
+de busqueda por ID, Clases, etc. ya que deprecaron. 
+
+Para ejecutarlos se debe instalar Python mínimo 3.8, la libreria Selenium en la versión mas reciente y para no tener 
+problemas de ChromeDriver instalar la biblioteca ChromeDriverManager.
---
+++ Job/SeleniumPython-Job/SeleniumPythonMoodle/SeleniumPython/BotExpCal.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# Librerias
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from csv import reader
+
+#Inicio del Navegador
+start = webdriver.ChromeOptions()
+start.add_argument('--start-maximized')
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+#Inicio de Sesion en Moodle
+    #Credenciales e ID del curso
+User = ""
+Pass = ""
+CursosID = []
+Clave = 'Total Cognitivo Bimestre 1'
+
+#     #Script login y administración
+driver.get("https://formarinnovar.com/moodle")
+
+WebDriverWait(driver,(1)).until(EC.presence_of_element_located((By.ID, "password")))
+
+input_User = driver.find_element(By.ID, 'username')
+input_Pass = driver.find_element(By.ID, 'password')
+input_User.send_keys(User)
+input_Pass.send_keys(Pass)
+SubmitB = driver.find_element(By.ID, "loginbtn")
+SubmitB.click()
+
+    #Entrar a la interfaz de exportar calificaciones del Curso.
+def CursoCatTree(CursoId):
+ driver.get("https://formarinnovar.com/moodle/grade/export/xls/index.php?id=" + str(CursoID))
+ WebDriverWait(driver, 10)
+    
+def SelCategorias():
+    #Deseleccionar categorias
+ WebDriverWait(driver, 10)
+ DeselectCats = driver.find_element(By.LINK_TEXT, "Seleccionar todos/ninguno")
+ DeselectCats.click()
+ driver.execute_script("window.scrollTo({ top: 0});")
+ #Crea arreglo de los elementos de la clase.
+ SelectCatsClass = driver.find_elements(By.CLASS_NAME, "form-check.d-flex")
+ #Busca el indice de la clave.
+ IndexClave = 0
+ for i in range (0, len(SelectCatsClass)):
+    if SelectCatsClass[i].find_element(By.TAG_NAME, 'label').text == Clave:
+        IndexClave = i
+        print (IndexClave)
+        break
+ #Selecciona los demas componetes del bimestre
+ for i in range (0, 5): 
+    SelectCatsClass[IndexClave].find_element(By.TAG_NAME,'label').click()
+    IndexClave = IndexClave + 1
+
+def Download():
+    driver.find_element(By.ID, "id_submitbutton").click()
+    
+
+
+for CursoID in CursosID:
+ CursoCatTree(CursoID)
+ SelCategorias()
+ Download()
+ print(CursoID)
+print("Finalizado")
+
